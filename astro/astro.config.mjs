cb438171482a8c3cf02b65dc8cd845632338130e
Sebastian Beltran <bjohansebas@gmail.com>
2026-02-22 00:40:35 +0000

    feat: add remark plugin to rewrite localized links in Markdown content

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
+import remarkRewriteLocalizedLinks from './src/utils/remark/rewrite-localized-links.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), icon()],
   markdown: {
+    remarkPlugins: [[remarkRewriteLocalizedLinks, { prefixes: ['guide', 'starter', 'api'] }]],
     shikiConfig: {
       theme: 'github-dark',
     },
